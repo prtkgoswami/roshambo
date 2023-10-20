e1a3275d68d52c4ea18adabf935e6e5d1be5b26e
Pratik Goswami <prtkgoswami8@gmail.com>
2023-10-20 02:00:47 -0700

    Fix BG Color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GameController from "./GameController";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col px-5 py-5 lg:px-5 lg:py-5 gap-3 lg:gap-10 relative">
+    <main className="flex min-h-screen w-full flex-col px-5 py-5 lg:px-5 lg:py-5 gap-3 lg:gap-10 relative bg-zinc-950">
       <header className="flex flex-row w-full justify-center lg:justify-start">
         <div className="flex flex-col p-2 lg:p-4 border-0 border-solid border-zinc-200/40">
           <div className="text-4xl lg:text-6xl uppercase py-2 border-b border-solid border-zinc-200/70 text-center lg:text-left">
